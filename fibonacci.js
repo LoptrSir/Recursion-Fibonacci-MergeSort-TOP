2b6f04861cc8c60225d890091880e7b74be7516b
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2024-04-26 13:13:21 -0600

    cleared console.logs

--- fibonacci.js
+++ fibonacci.js
@@ -1,26 +1,24 @@
 //TOP Recursion assignments
-    //View results in DevTools console
+//View results in DevTools console
 
 //Fibonacci Sequence:  Iterative and recursive examples.
-    //**Iterative Version**
-    function iterativeFibonacci(nbr){
-        // console.log('function called')
-        let fibArray = [0,];
-        let a = 0;
-        let b = 1;
-        if (nbr > 0) {
-            for(let i = 0; i < (nbr -1); i++){
-                fibArray.push(a+b);
-                a = b;
-                b = fibArray[i];
-                // console.log('loopResult:', fibArray);
-            }
-        }
-        return fibArray;
+//**Iterative Version**
+function iterativeFibonacci(nbr) {
+  let fibArray = [0];
+  let a = 0;
+  let b = 1;
+  if (nbr > 0) {
+    for (let i = 0; i < nbr - 1; i++) {
+      fibArray.push(a + b);
+      a = b;
+      b = fibArray[i];
     }
-console.log('IterativeFibonacci:', iterativeFibonacci(15));
+  }
+  return fibArray;
+}
+console.log("IterativeFibonacci:", iterativeFibonacci(15));
 //**
-    //**Recursive Version**
+//**Recursive Version**
 function generateRecursiveFibonacci(n, fibArray = [0, 1]) {
   if (n <= 0) {
     return []; // Base case: Return an empty array for non-positive input
@@ -30,12 +28,13 @@ function generateRecursiveFibonacci(n, fibArray = [0, 1]) {
     return fibArray; // Base case: Return [0, 1] for n = 2
   } else if (fibArray.length < n) {
     // Recursive case: Continue generating Fibonacci numbers until the array length reaches n
-    fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]);
+    fibArray.push(
+      fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]
+    );
     return generateRecursiveFibonacci(n, fibArray); // Recursively call the function with updated array and remaining count
-   } else {
-     return fibArray; // Return the generated Fibonacci array when the array length reaches n
+  } else {
+    return fibArray; // Return the generated Fibonacci array when the array length reaches n
   }
 }
-console.log('RecursiveFibonacci:', generateRecursiveFibonacci(11));
+console.log("RecursiveFibonacci:", generateRecursiveFibonacci(11));
 //**
-
--- mergeSort.js
+++ mergeSort.js
@@ -4,23 +4,17 @@
 function mergeSort(list) {
   if (list.length <= 1) {
     return list;
-    console.log("MS <+ 1:", list);
-  } else {
+   } else {
     let breakupArray = list.length / 2;
-    console.log("breakup:", breakupArray, "list:", list);
     let leftArray = list.slice(0, breakupArray);
     let rightArray = list.slice(breakupArray);
-    console.log("leftArray:", leftArray, "rightArray:", rightArray);
     leftArray = mergeSort(leftArray);
-    console.log("MS leftArray:", leftArray, "right:", rightArray);
     rightArray = mergeSort(rightArray);
-    console.log("MS rightArray:", rightArray, "left:", leftArray);
     return merge(leftArray, rightArray);
   }
 }
 
 function merge(left, right) {
-  console.log("merge l/r:", left, right);
   let mergedArray = [];
   let leftIndex = 0;
   let rightIndex = 0;
@@ -29,29 +23,22 @@ function merge(left, right) {
     if (left[leftIndex] <= right[rightIndex]) {
       mergedArray.push(left[leftIndex]);
       leftIndex++;
-      console.log("merge l<:","li:",leftIndex,"ri:",rightIndex,"MergedArray:",mergedArray);
     } else {mergedArray.push(right[rightIndex]);
     rightIndex++;
-    console.log("merge r<:","li:",leftIndex,"ri:",rightIndex,"MergedArray:",mergedArray);
     }
 }
     while (leftIndex < left.length) {
       mergedArray.push(left[leftIndex]);
       leftIndex++;
-      console.log("leftIndex remainder:",leftIndex,"MergedArray:", mergedArray);
     }
     while (rightIndex < right.length) {
       mergedArray.push(right[rightIndex]);
       rightIndex++;
-      console.log("rightIndexRemainder:",rightIndex,"MergedArray:", mergedArray);
     }
-    console.log("mergedArray:", mergedArray);
     return mergedArray;
  }
 
 let listA = [3, 2, 1, 13, 8, 5, 0, 1, -1];
-// let listA = [3, 2, 13, 1, 9];
-//console.log("listA:", mergeSort(listA));
 let listB = [105, 79, 100, 110];
 console.log('listA:', mergeSort(listA), 'listB:', mergeSort(listB));
 
